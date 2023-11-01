152a89bdaed9e75231db1ecd3ddc1f34de8a50b1
Rene Ravanan <83232378+Renz456@users.noreply.github.com>
2023-11-01 10:11:25 -0400

    Added fixed point sim

--- nbody.h
+++ nbody.h
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "ap_fixed.h"
 
-#define INPUT_LENGTH (15)
-typedef ap_fixed<16, 8, > fixed_t;
-
+#define INPUT_LENGTH (30)
+// typedef ap_fixed<16,8,  AP_RND, AP_WRAP> fixed_t;
+typedef ap_fixed<8, 4> fixed_t;
 extern "C"
 {
   // void nBodySimulation2D(float *particles);
-  void nBodySimulation2D(float *particles, float *temp);
+  void nBodySimulation2D(fixed_t *particles, fixed_t *temp);
 }
--- nbody_test.cpp
+++ nbody_test.cpp
@@ -4,27 +4,28 @@
 */
 
 #include "nbody.h"
+#include <cmath>
 
 // ********************************************************
 int main()
 {
     printf("Starting Nbody sims....\n");
-    float particles[75];
-    float original_particles[75];
-    float temp[75];
-    float a;
-    float b;
-    float c;
-    float d;
-    float e;
+    fixed_t particles[150];
+    fixed_t original_particles[150];
+    fixed_t temp[150];
+    fixed_t a;
+    fixed_t b;
+    fixed_t c;
+    fixed_t d;
+    fixed_t e;
 
-    for (int i = 0; i < 75; i += 5)
+    for (int i = 0; i < 150; i += 5)
     {
-        a = 0.1f * i;
-        b = 0.2f * i;
-        c = 0.3f * i;
-        d = 0.4f * i;
-        e = 0.5f * i;
+        a = 0.1 * i;
+        b = 0.2 * i;
+        c = 0.3 * i;
+        d = 0.4 * i;
+        e = 0.5 * i;
 
         particles[i] = a;
         particles[i + 1] = b;
@@ -43,7 +44,7 @@ int main()
     nBodySimulation2D(particles, temp);
 
     bool passed = false;
-    for (int i = 0; i < 75; i++)
+    for (int i = 0; i < 150; i++)
     {
         if (original_particles[i] != particles[i])
         {
@@ -59,6 +60,27 @@ int main()
     {
         printf("Nbody failed!\n");
     }
+
+    // Declare variables of the custom fixed-point type
+    // fixed_t customValue1 = 2.5;
+    // fixed_t customValue2 = 1.150;
+
+    // // Perform arithmetic operations with custom fixed-point numbers
+    // fixed_t customSum = customValue1 + customValue2;
+    // fixed_t customProduct = customValue1 * customValue2;
+    // double customSqrtValue = std::sqrt(customProduct.to_double());
+    // fixed_t test = static_cast<fixed_t>(customSqrtValue);
+    // // Convert custom fixed-point numbers to double for printing
+    // double customValue1_double = customValue1.to_double();
+    // double customValue2_double = customValue2.to_double();
+    // double customSum_double = customSum.to_double();
+    // double customProduct_double = customProduct.to_double();
+
+    // // Print the results
+    // std::cout << "customValue1 = " << customValue1_double << " (" << customValue1 << ")" << std::endl;
+    // std::cout << "customValue2 = " << customValue2_double << " (" << customValue2 << ")" << std::endl;
+    // std::cout << "customValue1 + customValue2 = " << customSum_double << " (" << customSum << ")" << std::endl;
+    // std::cout << "customValue1 * customValue2 = " << customProduct_double << " (" << customProduct << ")" << std::endl;
 
     return 0;
 }
