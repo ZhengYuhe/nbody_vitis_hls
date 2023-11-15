e668942ea69f7261b7a6579797408e270704994f
Rene Ravanan <83232378+Renz456@users.noreply.github.com>
2023-11-15 16:51:06 -0500

    working pipeline and unroll

--- nbody.cpp
+++ nbody.cpp
@@ -5,26 +5,30 @@ void nBodySimulation2D(float particles[INPUT_LENGTH * 5], float temp[INPUT_LENGT
     const float time_step = 0.01;
     const float G = 6.67430e-11f;
     const float min_cul_radius = 0.25;
-// fixed_t test = 1.0;
-#pragma HLS pipeline off
+    // fixed_t test = 1.0;
+    // #pragma HLS pipeline off
     for (int i = 0; i < INPUT_LENGTH; i += 5)
     {
-#pragma HLS unroll factor = 5000
+#pragma HLS unroll factor = 50
         // #pragma HLS task
-
-#pragma HLS pipeline off // II=10000
+        float x1 = particles[i];
+        float y1 = particles[i + 1];
+        float vx = particles[i + 2];
+        float vy = particles[i + 3];
+        float mass1 = particles[i + 4];
         float force_x = 0;
         float force_y = 0;
         for (int j = 0; j < INPUT_LENGTH; j += 5)
         {
+#pragma HLS pipeline II = 3
             if (i != j)
             {
                 // CalculateForce2D(particleData, i, j, force_x, force_y);
-                float x1 = particles[i];
-                float y1 = particles[i + 1];
+                //                float x1 = particles[i];
+                //                float y1 = particles[i + 1];
                 float x2 = particles[j];
                 float y2 = particles[j + 1];
-                float mass1 = particles[i + 4];
+                //                float mass1 = particles[i + 4];
                 float mass2 = particles[j + 4];
                 // Calculate the distance between the two particle      s in 2D
                 float dx = x2 - x1;
@@ -46,27 +50,22 @@ void nBodySimulation2D(float particles[INPUT_LENGTH * 5], float temp[INPUT_LENGT
             }
         }
 
-        float x1 = particles[i];
-        float y1 = particles[i + 1];
-        float vx = particles[i + 2];
-        float vy = particles[i + 3];
-        float mass = particles[i + 4];
         // Calculate acceleration in 2D
-        float ax = force_x / mass;
-        float ay = force_y / mass;
+        float ax = force_x / mass1;
+        float ay = force_y / mass1;
         // Update velocity in 2D using the calculated acceleration and time step
         temp[i + 2] = vx + ax * time_step;
         temp[i + 3] = vy + ay * time_step;
         temp[i] = x1 + temp[i + 2] * time_step;
         temp[i + 1] = y1 + temp[i + 3] * time_step;
-        temp[i + 4] = mass;
+        temp[i + 4] = mass1;
         // test *= 2;
         // printf("Check out this var %f\n", static_cast<fixed_t>(test));
     }
-
     for (int i = 0; i < INPUT_LENGTH; i += 5)
     {
-#pragma HLS unroll factor = 5000
+#pragma HLS pipeline off
+#pragma HLS unroll factor = 50
         particles[i] = temp[i];
         particles[i + 1] = temp[i + 1];
         particles[i + 2] = temp[i + 2];
